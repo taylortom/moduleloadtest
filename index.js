778531e0974b5cf181c4ff92282499ec3927c190
Thomas Taylor <thomas.taylor@kineo.com>
2019-12-09 17:08:50 +0000

    Catch error test

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const { DependencyLoader } = require('adapt-authoring-core');
 
 async function test() {
   const d = new DependencyLoader();
-  await d.load();
-  console.log('done load', d.dependencies);
+  try {
+    await d.load();
+    console.log('index.js: done load', d.dependencies);
+  } catch(e) {
+    console.log('index.js: load error', e);
+  }
 }
 test();
