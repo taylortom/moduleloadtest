8d1b3bf980eb196e2d7c58ae78639f9ab15c3e76
Thomas Taylor <thomas.taylor@kineo.com>
2019-12-10 16:09:23 +0000

    Change scope of vars for better readability

--- node_modules/adapt-authoring-core/lib/events.js
+++ node_modules/adapt-authoring-core/lib/events.js
@@ -1,38 +1,84 @@
 const EventEmitter = require('events');
-/** @ignore */
-const emitter = new EventEmitter();
-
+/** @ignore */ const _staticEmitter = new EventEmitter();
 /**
 * Adds custom event handling to objects
 */
 class Events {
   /**
-   * Trigger a static event
-   */
-  static emit(...args) {
-    emitter.emit(...args);
+  * Add an observer to a static event
+  * @param {String} eventName
+  * @param {Function} listener Callback function
+  * @see https://nodejs.org/api/events.html#events_emitter_on_eventname_listener
+  */
+  static on(eventName, listener) {
+    _staticEmitter.on(eventName, listener);
   }
   /**
-   * Add an observer to a static event
-   */
-  static on(...args) {
-    emitter.on(...args);
+  * Add an observer to a static event
+  * @param {String} eventName
+  * @param {Function} listener Callback function
+  * @see https://nodejs.org/api/events.html#events_emitter_on_eventname_listener
+  */
+  static once(eventName, listener) {
+    _staticEmitter.once(eventName, listener);
+  }
+  /**
+  * Remove an observer to a static event
+  * @param {String} eventName
+  * @param {Function} listener Callback function
+  * @see https://nodejs.org/api/events.html#events_emitter_on_eventname_listener
+  */
+  static off(eventName, listener) {
+    _staticEmitter.off(eventName, listener);
+  }
+  /**
+  * Trigger a static event
+  * @param {...*} rest Arguments to be passed to listeners
+  * @see https://nodejs.org/api/events.html#events_emitter_emit_eventname_args
+  */
+  static emit(eventName, ...rest) {
+    _staticEmitter.emit(eventName, ...rest);
   }
   /**
    * Creates a new Event instance
    */
   constructor(...args) {
-    const emitter = new EventEmitter();
-    /**
-    * Trigger an instance event
-    * @see https://nodejs.org/api/events.html#events_emitter_emit_eventname_args
-    */
-    this.emit = emitter.emit;
-    /**
-    * Add an observer to an instance event
-    * @see https://nodejs.org/api/events.html#events_emitter_on_eventname_listener
-    */
-    this.on = emitter.on;
+    this._emitter = new EventEmitter();
+  }
+  /**
+  * Add an observer to an instance event
+  * @param {String} eventName
+  * @param {Function} listener Callback function
+  * @see https://nodejs.org/api/events.html#events_emitter_on_eventname_listener
+  */
+  on(eventName, listener) {
+    this._emitter.on(eventName, listener);
+  }
+  /**
+  * Add an observer to an instance event
+  * @param {String} eventName
+  * @param {Function} listener Callback function
+  * @see https://nodejs.org/api/events.html#events_emitter_on_eventname_listener
+  */
+  once(eventName, listener) {
+    this._emitter.once(eventName, listener);
+  }
+  /**
+  * Remove an observer to an instance event
+  * @param {String} eventName
+  * @param {Function} listener Callback function
+  * @see https://nodejs.org/api/events.html#events_emitter_on_eventname_listener
+  */
+  off(eventName, listener) {
+    this._emitter.off(eventName, listener);
+  }
+  /**
+  * Trigger an instance event
+  * @param {...*} rest Arguments to be passed to listeners
+  * @see https://nodejs.org/api/events.html#events_emitter_emit_eventname_args
+  */
+  emit(eventName, ...rest) {
+    this._emitter.emit(eventName, ...rest);
   }
 }
 
