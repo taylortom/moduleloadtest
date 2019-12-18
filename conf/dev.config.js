a4189ec45dce71e83842b5915c4d68482bdcde6c
Thomas Taylor <thomas.taylor@kineo.com>
2019-12-18 15:56:31 +0000

    Update config module

---
+++ conf/dev.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  'sapp': {
+    local_modules_path: "/Users/tom/Projects/adapt_authoring_restructure"
+  },
+  'adapt-authoring-jsonschemaa': {
+    formatOverrides: {
+      "date": /.+/
+    }
+  },
+  'adapt-authoring-lint': {
+    rules: {
+    // Possible Errors
+      "no-console": 2,
+      "no-template-curly-in-string": 1,
+    // Best Practices
+    // Variables
+      "no-unused-vars": 1
+    // Stylistic Issues
+    // ECMAScript 6
+    }
+  },
+  'adapt-authoring-logger': {
+    enabledLevels: ['error', 'warn', 'success', 'info', 'debug'],
+    showTimestamp: true
+  },
+  'adapt-authoring-mongodb': {
+    host: "localhost",
+    port: 27017,
+    dbname: "adapt-authoring-prototype"
+  },
+  'adapt-authoring-server': {
+    host: "localhost",
+    port: 5678,
+    url: 'http://localhost:5678',
+    logStackOnError: true
+  }
+};
