29280ca3e11108b5e6203ac47be28e841ecf7bf1
Thomas Taylor <thomas.taylor@kineo.com>
2019-12-18 15:56:47 +0000

    Update log

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const App = require('adapt-authoring-core').App;
 async function test() {
   App.instance;
   const app = await App.instance.onReady();
-  console.log('done', app);
+  console.log('done');
 }
 
 
