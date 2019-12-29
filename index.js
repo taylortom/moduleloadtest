5f65c7ec6a23923326482e9e664ac66c9b8b5de5
Thomas Taylor <thomas.taylor@kineo.com>
2019-12-29 17:24:33 +0000

    Add default env

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { App } = require('adapt-authoring-core');
 
 async function test() {
+  if(!process.env.NODE_ENV) {
+    process.env.NODE_ENV = 'dev';
+  }
   await App.instance.onReady();
 }
 
